cfe5899b5c89356fac0f08be751fb26fd74eab0a
jhonascba <jhonascbarbosa@gmail.com>
2023-03-29 20:42:31 -0300

    first commit

---
+++ src/lib/components/IngredienteSelecionavel.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import Tag from "$component/Tag.svelte";
+    import {createEventDispatcher} from "svelte";
+
+    const dispatch = createEventDispatcher<{
+        adicionarIngrediente: string;
+        removerIngrediente: string;
+    }>();
+
+    export let ingrediente: string;
+
+    let selecionado = false;
+
+    function aoClicar() {
+        selecionado = !selecionado;
+
+        if (selecionado) {
+            dispatch('adicionarIngrediente', ingrediente);
+        } else {
+            dispatch('removerIngrediente', ingrediente);
+        }
+    }
+</script>
+
+<button on:click={aoClicar}>
+    <Tag ativa={selecionado}>{ingrediente}</Tag>
+</button>
+
+<style>
+    button {
+        cursor: pointer;
+    }
+</style>
